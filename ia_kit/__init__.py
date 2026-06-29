12084cf3585eba1158de9165478d2c45f03f1569
Pablo Fiscella <pablofiscella@gmail.com>
2026-06-28 21:29:50 -0300

    test: harness pytest + paquete ia_kit

---
+++ ia_kit/__init__.py
@@ -0,0 +1,1 @@
+"""Motor de generación de piezas con IA (OpenAI Images). Solo stdlib + Pillow."""
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+import ia_kit
+
+
+def test_paquete_importa():
+    assert ia_kit.__doc__ is not None
